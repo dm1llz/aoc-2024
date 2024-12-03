b3e3362569f06179ab31c6184032068e830c7926
David Miller <pc_freak@att.net>
2024-12-02 21:06:00 -0800

    chore: reduced duplication

--- test/day_1_test.exs
+++ test/day_1_test.exs
@@ -2,8 +2,7 @@ defmodule Day1Test do
   use ExUnit.Case
   doctest Aoc2024.Day1
 
-  test "part 1 input is 11" do
-    input = """
+  @input """
     3   4
     4   3
     2   5
@@ -12,19 +11,11 @@ defmodule Day1Test do
     3   3
     """
 
-    assert Aoc2024.Day1.part1(input) == 11
+  test "part 1 input is 11" do
+    assert Aoc2024.Day1.part1(@input) == 11
   end
 
-  test "part 1 input is 31" do
-    input = """
-    3   4
-    4   3
-    2   5
-    1   3
-    3   9
-    3   3
-    """
-
-    assert Aoc2024.Day1.part2(input) == 31
+  test "part 2 input is 31" do
+    assert Aoc2024.Day1.part2(@input) == 31
   end
 end
--- test/day_2_test.exs
+++ test/day_2_test.exs
@@ -2,8 +2,7 @@ defmodule Day2Test do
   use ExUnit.Case
   doctest Aoc2024.Day2
 
-  test "part 1 input is 2" do
-    input = """
+  @input """
     7 6 4 2 1
     1 2 7 8 9
     9 7 6 2 1
@@ -12,19 +11,11 @@ defmodule Day2Test do
     1 3 6 7 9
     """
 
-    assert Aoc2024.Day2.part1(input) == 2
+  test "part 1 input is 2" do
+    assert Aoc2024.Day2.part1(@input) == 2
   end
 
-  test "part 1 input is 4" do
-    input = """
-    7 6 4 2 1
-    1 2 7 8 9
-    9 7 6 2 1
-    1 3 2 4 5
-    8 6 4 4 1
-    1 3 6 7 9
-    """
-
-    assert Aoc2024.Day2.part2(input) == 4
+  test "part 2 input is 4" do
+    assert Aoc2024.Day2.part2(@input) == 4
   end
 end
